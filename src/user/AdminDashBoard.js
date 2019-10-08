5cc43f5fff912dbef0098e0bbf31bea9f3d4935f
werachet <chet_usa@hotmail.com>
2019-10-08 09:33:02 -0700

    completed ready to deploy

--- src/user/AdminDashBoard.js
+++ src/user/AdminDashBoard.js
@@ -20,6 +20,9 @@ function AdminDashBoard() {
           <li className="list-group-item">
           <Link className="nav-link" to="/admin/orders"> View Orders </Link>
           </li>
+          <li className="list-group-item">
+          <Link className="nav-link" to="/admin/products"> Manage Products </Link>
+          </li>
         </ul>
       </div>
     )
